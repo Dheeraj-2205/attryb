b7251af25c1d02c9ad9ffc9843e6c674d2f1a980
Dheeraj-2205 <djsays124@gmail.com>
2023-11-04 03:34:03 +0530

    delete and update froadmin

--- attryb/backend/controller/oem_specs.js
+++ attryb/backend/controller/oem_specs.js
@@ -4,7 +4,6 @@ exports.createProduct = async(req,res) =>{
     try {
 
         const product = await oemSpecs.create(req.body)
-
         return res.status(201).json({
             success : true,
             product
@@ -36,11 +35,27 @@ exports.getAllProducts = async(req,res)=>{
     }
 }
 
+exports.adminGetAllProducts = async(req,res)=>{
+    try {
+        const products = await oemSpecs.find();
+
+        return res.status(200).json({
+            success : true,
+            products
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            error : error.message
+        })
+    }
+}
 
 exports.updateProducts = async(req,res)=>{
     try {
-        let product = await oemSpecs.findById(req.params.id);
 
+        let product = await oemSpecs.findById(req.params.id);
+        
         if(!product){
             return res.status(400).json({
                 success : false,
@@ -48,15 +63,49 @@ exports.updateProducts = async(req,res)=>{
             })
         };
     
-        product = await product.findByIdAndUpdate (req.params.id ,req.body, {
+        product = await oemSpecs.findByIdAndUpdate( req.params.id ,req.body, {
             new : true,
-            useFindAndModify : false
+            runValidators : true,
+            useFindAndModify: false,
         });
     
         return res.status(200).json({
             success : true,
             product
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            error : error.message
         })
+    }
+};
+
+
+exports.deleteProducts = async(req,res) =>{
+    try {
+        const product = await oemSpecs.findById(req.params.id);
+        console.log(product)
+
+        if (!product) {
+          return res.status(400).json({
+            success : true,
+            error : "Product Is Not Found"
+          })
+        }
+      
+        
+        // for (let i = 0; i < product.images.length; i++) {
+        //   await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+        // }
+      
+        await product.deleteOne();
+      
+        res.status(200).json({
+          success: true,
+          message: "Product Delete Successfully",
+        });
     } catch (error) {
         return res.status(500).json({
             success : false,
--- attryb/backend/routes/product.js
+++ attryb/backend/routes/product.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createProduct, getAllProducts } = require('../controller/oem_specs');
+const { createProduct, getAllProducts, updateProducts, adminGetAllProducts,deleteProducts } = require('../controller/oem_specs');
 const router = express.Router();
 
 router.route("/newoem").post(createProduct)
 router.route("/alloem").get(getAllProducts)
+router.route("/admin/getall").get(adminGetAllProducts)
+router.route("admin/:id").put(updateProducts).delete(deleteProducts);
 
 
 module.exports = router
