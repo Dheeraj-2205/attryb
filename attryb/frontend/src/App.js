8efaf5085224187fcfccaeb9b55a7e474cc1f6e9
Dheeraj-2205 <djsays124@gmail.com>
2023-11-04 16:12:40 +0530

    adding css file

--- attryb/frontend/src/App.js
+++ attryb/frontend/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/login" element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ attryb/frontend/src/components/Signup/Signup.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import style from "./Signup.module.css"
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+import env from "react-dotenv"
+
+const Signup = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userData = {
+        name: state.name,
+        email: state.email,
+        password: state.password,
+      };
+      console.log(userData)
+
+      const res = await fetch(`http://localhost:8000/api/v1/register`,{
+        method : 'POST',
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(userData)
+      })
+      
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <form onSubmit={handleSubmit}>
+        <label> Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Your Name"
+          onChange={handleInputChange}
+          value={state.name}
+        />
+        <label> Email</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter Your Email"
+          onChange={handleInputChange}
+          value={state.email}
+        />
+        <label> Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Your Password"
+          onChange={handleInputChange}
+          value={state.password}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
